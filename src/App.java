1606af870e6cade2829f9d76dd7254b6f1e546a8
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-06-03 10:11:57 -0400

    optimizatoins

--- src/App.java
+++ src/App.java
@@ -1,20 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        //get the current time
+        long startTime = System.currentTimeMillis();
+        String pString = "";
         // for each number print out fizz if it is divisible by 3, buzz if it is divisible by 5, fizzbuzz if it is divisible by both 3 and 5, or the number itself otherwise
         for (int i = 1; i < 100; i ++){
             if (i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
+                pString += "FizzBuzz\n";
             }
             else if (i % 3 == 0){
-                System.out.println("Fizz");
+                pString += "Fizz\n";
             }
             else if (i % 5 == 0){
-                System.out.println("Buzz");
+                pString += "Buzz\n";
             }
             else {
-                System.out.println(i);
+                pString += i + "\n";
             }
         }
+        System.out.println(pString);
+
+        //print out the time it took to run
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) + "ms");
     }
 }
