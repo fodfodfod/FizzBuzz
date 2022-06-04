63ac49b7a0c382c23474fde34c817d6c368b9dbe
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-06-04 17:15:06 -0400

    working multithreading

--- src/App.java
+++ src/App.java
@@ -3,26 +3,37 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    
+        public static void main(String[] args) throws Exception {
         //get the current time
         long startTime = System.currentTimeMillis();
-        String pString = "";
-        // for each number print out fizz if it is divisible by 3, buzz if it is divisible by 5, fizzbuzz if it is divisible by both 3 and 5, or the number itself otherwise
-        for (int i = 0; i < 100000; i ++){
-            String numberString = "";
-            if (i % 3 == 0){
-                numberString += "Fizz";
-            }
-            if (i % 5 == 0){
-                numberString += "Buzz";
-            }
-            if (numberString.equals("")){
-                numberString += i;
-            }
-            pString += numberString + "\n";
+        
+        StringBuilder t2String = new StringBuilder();
+        StringBuilder t3String = new StringBuilder();
+        StringBuilder t4String = new StringBuilder();
+        StringBuilder t1String = new StringBuilder();
+        //create 4 threads
+        Thread t1 = new Thread(new FizzBuzz(1, 25000, t1String));
+        Thread t2 = new Thread(new FizzBuzz(25001, 50000, t2String));
+        Thread t3 = new Thread(new FizzBuzz(50001, 75000, t3String));
+        Thread t4 = new Thread(new FizzBuzz(75001, 100000, t4String));
+        //start the threads
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        //join the threads to main thread
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
 
-        }
-        System.out.println(pString);
+        
+
+        //combine the strings and print them out
+        String pString = t1String.toString() + t2String.toString() + t3String.toString() + t4String.toString();
+        // System.out.print(t1String);
+        // System.out.print(pString);
 
         //print out the time it took to run
         long endTime = System.currentTimeMillis();
@@ -36,4 +47,5 @@ public class App {
         bw.close();
         
     }
+
 }
---
+++ src/FizzBuzz.java
@@ -0,0 +1,31 @@
+public class FizzBuzz implements Runnable{
+    private int start;
+    private int end;
+    private StringBuilder pString;
+    public FizzBuzz(int start, int end, StringBuilder pString) {
+        this.start = start;
+        this.end = end;
+        this.pString = pString;
+        pString.append("hi");
+    }
+
+    @Override
+    public void run() {
+        
+        System.out.println("hi");
+        for (int i = start; i <= end; i ++){
+            String numberString = "";
+            if (i % 3 == 0){
+                numberString += "Fizz";
+            }
+            if (i % 5 == 0){
+                numberString += "Buzz";
+            }
+            if (numberString.equals("")){
+                numberString += i;
+            }
+            pString.append(numberString + "\n");
+        }
+        // System.out.print(pString);
+    }
+}
