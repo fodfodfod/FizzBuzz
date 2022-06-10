d602729fb7c1b8ea484b4db4609b7d7b71b476ae
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-06-10 17:59:59 -0400

    more changes

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+// .jar command: java -Xmx12g -jar FizzBuzz.jar;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -8,7 +9,7 @@ public class App {
         
         /**max number of threads allowed to exist at one time(not including main thread) */
         final int threadCount = 10;
-        final int range = 100000000;
+        final int range = 1100000000;
         final int chunkSize = 100000;
 
         //get the current time
@@ -17,7 +18,7 @@ public class App {
 
 
         //reset the file
-        File tempFile = new File("output.txt");
+        File tempFile = new File("output-" + range + ".txt");
         tempFile.delete();
         StringBuilder[] threadStrings = new StringBuilder[(range/chunkSize) + 1];
         
@@ -37,13 +38,26 @@ public class App {
         }
         
         System.out.println("Initial threads started");
-        //join the threads to main thread
-        for (int i = 0; i < threads.length; i++){
-            threads[i].join();
-            threadsRunning--;
-            System.out.println("Thread " + i + " finished");
+    
+        //create the file to appened to
+        File outputFile = new File("output-" + range + ".txt");
+        FileWriter fw = new FileWriter(outputFile, true);
+        BufferedWriter bw = new BufferedWriter(fw);
 
+
+        for (int i = 0; i < threads.length; i++){
+            //create a variable to mesure the time spent waiting for the thread to finish
+            // long startTime2 = System.currentTimeMillis();
+            threads[i].join();
+            // long endTime2 = System.currentTimeMillis();
+            // System.out.println("Thread " + i + " finished in " + (endTime2 - startTime2) + " milliseconds");
+
+
+            threadsRunning--;
             
+
+            //create a variable to store the time creating a new thread
+            // long startTime3 = System.currentTimeMillis();
             //create a new thread if there are still threads to create
             if (threadsRunning < threadCount && threadsCreated < threads.length){
                 threadStrings[threadsCreated] = new StringBuilder();
@@ -53,20 +67,30 @@ public class App {
                 threadsCreated++;
                 threadsRunning++;
             }
-
+            // long endTime3 = System.currentTimeMillis();
+            // System.out.println("Thread " + (threadsCreated - 1) + " created in " + (endTime3 - startTime3) + " milliseconds");
 
             
+            //create a variable to store the time writing to the file
+            // long startTime4 = System.currentTimeMillis();
+
             //append the stringbuilder to the file
-            File file = new File("output.txt");
-            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
-            BufferedWriter bw = new BufferedWriter(fw);
             bw.write(threadStrings[i].toString());
-            bw.close();
             
+            
+            // long endTime4 = System.currentTimeMillis();
+            // System.out.println("Thread " + i + " wrote to file in " + (endTime4 - startTime4) + " milliseconds");
 
+            if(i%100==0){
+                System.out.println("things are happening");
+            }
             
-            
+            //delete the string builder
+            threadStrings[i] = null;
+            //delete the thread
+            threads[i] = null;
         }
+        bw.close();
         System.out.println("All threads joined");
  
         //combine the strings and print them out
@@ -77,7 +101,7 @@ public class App {
         // System.out.print(t1String);
         // System.out.print(pString);
  
-        //print out the time it took to run
+        
        
  
         //write the output to a file called output.txt
